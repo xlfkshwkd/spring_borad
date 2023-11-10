79b5006909f0ba167cf34e28dcc42e8f19aabb7a
xlfkshwkd <xlfkshwkd@naver.com>
2023-11-10 16:42:09 +0900

    spring file +

--- src/main/java/com/simsimhi/commons/rest/JSONData.java
+++ src/main/java/com/simsimhi/commons/rest/JSONData.java
@@ -2,12 +2,19 @@ package com.simsimhi.commons.rest;
 
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class JSONData<T> {
     private boolean success =true;
     private HttpStatus status =HttpStatus.OK;
+
+    @NonNull
     private T data;
     private String message;
 
